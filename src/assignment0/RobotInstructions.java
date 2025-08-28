9ab327c4a697df16115b6c34b0cac3084b539753
aerinpark <aerinjoypark@gmail.com>
2025-08-28 10:13:23 -0500

    Made robot draw a star

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -29,11 +29,20 @@ public class RobotInstructions  {
 		 * You will add instructions at the end of the code to cause the support.robot to move
 		 * as you desire.
 		 */
-		robot.forward(60);
-		robot.turnLeft(90);
-		robot.forward(23);
-		robot.turnRight(90);
-		robot.forward(80);
+		robot.forward(120);
+		robot.turnRight(55);
+		robot.forward(200);
+		robot.turnLeft(145);
+		robot.forward(190);
+		robot.turnLeft(145);
+		robot.forward(200);
+		robot.turnLeft(145);
+		robot.forward(200);
+		robot.turnLeft(145);
+		robot.forward(200);
+	
+		
+		
 	}
 
 }
